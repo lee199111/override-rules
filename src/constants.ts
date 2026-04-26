9df4f521d57e3d11211565a215e67766bad54fda
powerfullz <wleizc@hotmail.com>
2026-04-26 12:34:26 +0800

    feat(constants): optimize proxy region regex and add NL/TR regions
    - Implement strict word boundaries (\b) for English aliases to prevent substring misidentification (e.g., preventing "node" from matching "DE").
    - Add Netherlands (NL) and Turkey (TR) regions with accurate regex patterns and CDN icons.
    - Remove JavaScript-specific negative lookbehind in the Japan regex to ensure strict compatibility with Clash/Mihomo's Go RE2 engine.
    - Properly group lowercase alias variants inside word boundaries (e.g., \b(?:HK|hk)).

--- src/constants.ts
+++ src/constants.ts
@@ -41,74 +41,92 @@ export const PROXY_GROUPS = {
 export const countriesMeta: Record<string, CountryMeta> = {
     香港: {
         weight: 10,
-        pattern: "香港|港|HK|hk|Hong Kong|HongKong|hongkong|🇭🇰",
+        pattern:
+            "香港|港|\\b(?:HK|hk)(?:[-_ ]?\\d+(?:[-_ ]?[A-Za-z]{2,})?)?\\b|Hong Kong|HongKong|hongkong|HONG KONG|HONGKONG|深港|HKG|九龙|Kowloon|新界|沙田|荃湾|葵涌|🇭🇰",
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/Hong_Kong.png`,
     },
     澳门: {
-        pattern: "澳门|MO|Macau|🇲🇴",
+        pattern: "澳门|\\b(?:MO|mo)(?:[-_ ]?\\d+(?:[-_ ]?[A-Za-z]{2,})?)?\\b|Macau|🇲🇴",
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/Macao.png`,
     },
     台湾: {
         weight: 20,
-        pattern: "台|新北|彰化|TW|Taiwan|🇹🇼",
+        pattern:
+            "台|新北|彰化|\\b(?:TW|tw)(?:[-_ ]?\\d+(?:[-_ ]?[A-Za-z]{2,})?)?\\b|Taiwan|TAIWAN|TWN|TPE|ROC|🇹🇼|🇼🇸",
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/Taiwan.png`,
     },
     新加坡: {
         weight: 30,
-        pattern: "新加坡|坡|狮城|SG|Singapore|🇸🇬",
+        pattern:
+            "新加坡|坡|狮城|\\b(?:SG|sg)(?:[-_ ]?\\d+(?:[-_ ]?[A-Za-z]{2,})?)?\\b|Singapore|SINGAPORE|SIN|🇸🇬",
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/Singapore.png`,
     },
     日本: {
         weight: 40,
-        pattern: "日本|川日|东京|大阪|泉日|埼玉|沪日|深日|JP|Japan|🇯🇵",
+        pattern:
+            "日本|川日|东京|大阪|泉日|埼玉|沪日|深日|\\b(?:JP|jp)(?:[-_ ]?\\d+(?:[-_ ]?[A-Za-z]{2,})?)?\\b|Japan|JAPAN|JPN|NRT|HND|KIX|TYO|OSA|关西|Kansai|KANSAI|🇯🇵",
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/Japan.png`,
     },
     韩国: {
-        pattern: "KR|Korea|KOR|首尔|韩|韓|🇰🇷",
+        pattern:
+            "韩国|韩|韓|春川|Chuncheon|首尔|\\b(?:KR|kr)(?:[-_ ]?\\d+(?:[-_ ]?[A-Za-z]{2,})?)?\\b|Korea|KOREA|KOR|ICN|🇰🇷",
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/Korea.png`,
     },
     美国: {
         weight: 50,
-        pattern: "美国|美|US|United States|🇺🇸",
+        pattern:
+            "美国|美|波特兰|达拉斯|俄勒冈|凤凰城|费利蒙|硅谷|拉斯维加斯|洛杉矶|圣何塞|圣克拉拉|西雅图|芝加哥|纽约|亚特兰大|迈阿密|华盛顿|\\b(?:US|us)(?:[-_ ]?\\d+(?:[-_ ]?[A-Za-z]{2,})?)?\\b|United States|UnitedStates|UNITED STATES|USA|America|AMERICA|JFK|EWR|IAD|ATL|ORD|MIA|NYC|LAX|SFO|SEA|DFW|SJC|🇺🇸",
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/United_States.png`,
     },
     加拿大: {
-        pattern: "加拿大|Canada|CA|🇨🇦",
+        pattern:
+            "加拿大|渥太华|温哥华|卡尔加里|蒙特利尔|Montreal|\\b(?:CA|ca)(?:[-_ ]?\\d+(?:[-_ ]?[A-Za-z]{2,})?)?\\b|Canada|CANADA|CAN|YVR|YYZ|YUL|🇨🇦",
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/Canada.png`,
     },
     英国: {
         weight: 60,
-        pattern: "英国|United Kingdom|UK|伦敦|London|🇬🇧",
+        pattern:
+            "英国|伦敦|曼彻斯特|Manchester|\\b(?:UK|uk)(?:[-_ ]?\\d+(?:[-_ ]?[A-Za-z]{2,})?)?\\b|Britain|United Kingdom|UNITED KINGDOM|England|GBR|LHR|MAN|🇬🇧",
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/United_Kingdom.png`,
     },
     澳大利亚: {
-        pattern: "澳洲|澳大利亚|AU|Australia|🇦🇺",
+        pattern: "澳洲|澳大利亚|\\b(?:AU|au)(?:[-_ ]?\\d+(?:[-_ ]?[A-Za-z]{2,})?)?\\b|Australia|🇦🇺",
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/Australia.png`,
     },
     德国: {
         weight: 70,
-        pattern: "德国|德|DE|Germany|🇩🇪",
+        pattern:
+            "德国|德|柏林|法兰克福|慕尼黑|Munich|\\b(?:DE|de)(?:[-_ ]?\\d+(?:[-_ ]?[A-Za-z]{2,})?)?\\b|Germany|GERMANY|DEU|MUC|🇩🇪",
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/Germany.png`,
     },
     法国: {
         weight: 80,
-        pattern: "法国|法|FR|France|🇫🇷",
+        pattern:
+            "法国|法|巴黎|马赛|Marseille|\\b(?:FR|fr)(?:[-_ ]?\\d+(?:[-_ ]?[A-Za-z]{2,})?)?\\b|France|FRANCE|FRA|CDG|MRS|🇫🇷",
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/France.png`,
     },
     俄罗斯: {
-        pattern: "俄罗斯|俄|RU|Russia|🇷🇺",
+        pattern: "俄罗斯|俄|\\b(?:RU|ru)(?:[-_ ]?\\d+(?:[-_ ]?[A-Za-z]{2,})?)?\\b|Russia|🇷🇺",
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/Russia.png`,
     },
     泰国: {
-        pattern: "泰国|泰|TH|Thailand|🇹🇭",
+        pattern: "泰国|泰|\\b(?:TH|th)(?:[-_ ]?\\d+(?:[-_ ]?[A-Za-z]{2,})?)?\\b|Thailand|🇹🇭",
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/Thailand.png`,
     },
     印度: {
-        pattern: "印度|IN|India|🇮🇳",
+        pattern: "印度|\\b(?:IN|in)(?:[-_ ]?\\d+(?:[-_ ]?[A-Za-z]{2,})?)?\\b|India|🇮🇳",
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/India.png`,
     },
     马来西亚: {
-        pattern: "马来西亚|马来|MY|Malaysia|🇲🇾",
+        pattern: "马来西亚|马来|\\b(?:MY|my)(?:[-_ ]?\\d+(?:[-_ ]?[A-Za-z]{2,})?)?\\b|Malaysia|🇲🇾",
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/Malaysia.png`,
+    },
+    荷兰: {
+        pattern: "荷兰|\\b(?:NL|nl)(?:[-_ ]?\\d+(?:[-_ ]?[A-Za-z]{2,})?)?\\b|Netherlands|🇳🇱",
+        icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/Netherlands.png`,
+    },
+    土耳其: {
+        pattern: "土耳其|\\b(?:TR|tr)(?:[-_ ]?\\d+(?:[-_ ]?[A-Za-z]{2,})?)?\\b|Turkey|🇹🇷",
+        icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/Turkey.png`,
     },
 };
